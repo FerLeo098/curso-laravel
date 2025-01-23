4d767d55ddf7315622bb4a41ab4d9dddb6f47be9
Leo <fernandoleonel.gonzalez@axarnet.es>
2025-01-23 10:27:34 +0100

    Primera version de la prueba: agregar una nueva vista

---
+++ resources/views/about.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="Fernando Leonel Gonzalez Llapur">
+    <title>Prueba de Laravel</title>
+</head>
+<body>
+
+    <h1>Hola, Bienvenido a mi curso de Laravel</h1>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maxime deserunt veniam dignissimos, hic quisquam sapiente fugiat ab facilis explicabo quae rem temporibus nobis beatae, deleniti vero. A porro expedita impedit?</p>
+
+</body>
+</html>
