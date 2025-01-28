f9be79d46f9c310e53e8d5b15a9337162514a684
Leo <fernandoleonel.gonzalez@axarnet.es>
2025-01-28 09:07:55 +0100

    {commit_message}

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 
+
 Route::get('/', function () {
+    $person = [
+        "name" => "Leo",
+        "email" => "fernandoleonel.gonzalez@axarnet.es"
+    ];
+
+    dump($person);
     return view('welcome');
 });
 
